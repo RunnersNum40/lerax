2ca941591889ed0345df0f8f7ecbf9bc80ba69cc
Ted <p_t@fastmail.net>
2025-10-01 12:03:28 -0400

    Compatibility examples

---
+++ examples/sb3.py
@@ -0,0 +1,10 @@
+import equinox as eqx
+from stable_baselines3 import PPO
+
+from oryx.compatibility.gym import OryxEnv
+from oryx.env import CartPole
+
+env = OryxEnv(*eqx.nn.make_with_state(CartPole)())
+
+model = PPO("MlpPolicy", env, tensorboard_log="logs")
+model.learn(total_timesteps=2**18, progress_bar=True)
