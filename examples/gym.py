28d9e67dea4974fc59e67735ee4483021e9b7c2a
Ted <p_t@fastmail.net>
2025-09-30 23:21:22 -0400

    Add gymnasium wrapping env

---
+++ examples/gym.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+import equinox as eqx
+import gymnasium as gym
+from jax import random as jr
+
+from oryx.algorithm import PPO
+from oryx.compatibility.gym import GymnasiumEnv
+from oryx.policy import MLPActorCriticPolicy
+from oryx.wrapper import EpisodeStatistics, TimeLimit
+
+policy_key, learn_key = jr.split(jr.key(0), 2)
+
+gym_env = gym.make("CartPole-v1")
+env = EpisodeStatistics(TimeLimit(GymnasiumEnv(gym_env), max_episode_steps=512))
+policy = policy = MLPActorCriticPolicy(
+    env=env,
+    key=policy_key,
+)
+
+algo, state = eqx.nn.make_with_state(PPO)(
+    env=env,
+    policy=policy,
+)
+
+algo.learn(
+    state,
+    total_timesteps=2**18,
+    key=learn_key,
+    show_progress_bar=True,
+    tb_log_name=f"logs/{env.name}_{datetime.now().strftime("%H%M%S")}",
+)
