Update NCDE for correct gradients

lerax/algorithm/ppo.py
@@ -5,15 +5,25 @@ import jax
 import optax
 from jax import numpy as jnp
 from jax import random as jr
-from jaxtyping import Array, Float, Key, Scalar
+from jaxtyping import Array, Bool, Float, Key, PyTree, Scalar
 
 from lerax.buffer import RolloutBuffer
 from lerax.policy import (
     AbstractStatefulActorCriticPolicy,
+    NCDEActorCriticPolicy,
 )
 from lerax.utils import filter_scan
 
 from .on_policy import AbstractOnPolicyAlgorithm
+
+
+def any_nan_or_inf(tree: PyTree) -> Bool[Array, ""]:
+    return jnp.logical_not(
+        jax.tree.reduce_associative(
+            jnp.logical_and,
+            jax.tree.map(lambda leaf: jnp.all(jnp.isfinite(leaf)), tree),
+        )
+    )
 
 
 class PPOStats(eqx.Module):
@@ -184,21 +194,17 @@ class PPO(AbstractOnPolicyAlgorithm):
             self.entropy_loss_coefficient,
         )
 
+        if isinstance(grads, NCDEActorCriticPolicy):
+            grads = eqx.error_if(
+                grads, any_nan_or_inf(grads.encoder), "Non-finite encoder grads."
+            )
+
         updates, new_opt_state = self.optimizer.update(
             grads, opt_state, eqx.filter(policy, eqx.is_inexact_array)
         )
-
-        # Check if updates are nan or inf
-        values = jax.tree.leaves(jax.tree.map(jnp.ravel, updates))[0]
-        flat = jnp.array(values)
-        valid = jnp.all(jnp.isfinite(flat))
-        invalid = jnp.logical_not(valid)
-
-        updates = eqx.error_if(
-            updates, invalid, "Invalid gradients detected (NaN or Inf)."
-        )
-
+        updates = eqx.error_if(updates, any_nan_or_inf(updates), "Non-finite update.")
         policy = eqx.apply_updates(policy, updates)
+
         return policy, new_opt_state, stats
 
     def train_epoch(

lerax/model/ncde/ncde.py
@@ -4,15 +4,15 @@ from typing import Callable
 
 import diffrax
 import equinox as eqx
-import jax
 from jax import lax
 from jax import nn as jnn
 from jax import numpy as jnp
 from jax import random as jr
-from jaxtyping import Array, Bool, Float, Int, Key, PyTree, Shaped
+from jaxtyping import Array, Float, Int, Key, PyTree, Shaped
 
 from ..base_model import AbstractModelState, AbstractStatefulModel
 from .term import AbstractNCDETerm, MLPNCDETerm
+from .utils import safe_control
 
 type Coeffs = tuple[
     PyTree[Shaped[Array, "times-1 ?*shape"]],
@@ -25,12 +25,11 @@ type Coeffs = tuple[
 class NCDEState(AbstractModelState):
     ts: Float[Array, " n"]
     xs: Float[Array, " n input_size"]
-    zs: Float[Array, " n latent_size"]
 
 
 class AbstractNeuralCDE[
-    LatentType: Callable[[Array], Float[Array, " latent_size"]],
-    OutputType: Callable[[Array], Float[Array, " latent_size"]],
+    LatentType: Callable[[Float[Array, " out_size"]], Float[Array, " latent_size"]],
+    OutputType: Callable[[Float[Array, " latent_size"]], Float[Array, " out_size"]],
 ](
     AbstractStatefulModel[
         NCDEState,
@@ -65,53 +64,13 @@ class AbstractNeuralCDE[
     output: eqx.AbstractVar[OutputType]
     time_in_input: eqx.AbstractVar[bool]
 
-    state_size: eqx.AbstractVar[int]
-
-    @staticmethod
-    def valid_ts(ts: Float[Array, " n"]) -> Bool[Array, ""]:
-        """Check if the initial time is valid (not NaN or Inf)."""
-        return jnp.isfinite(ts[0])
-
-    @staticmethod
-    def valid_z0(zs: Float[Array, " latent_size"]) -> Bool[Array, ""]:
-        """Check if all latent state variables are valid (not NaN or Inf)."""
-        return jnp.isfinite(zs).all()
-
-    @staticmethod
-    def valid_coeffs(coeffs: Coeffs) -> Bool[Array, ""]:
-        """Check if the initial coefficients are valid (not NaN or Inf)."""
-        return jnp.all(
-            jnp.isfinite(
-                jnp.array(
-                    jax.tree.leaves(jax.tree.map(lambda x: jnp.take(x, 0), coeffs))
-                )
-            )
-        )
-
-    def coeffs(self, ts: Float[Array, " n"], xs: Float[Array, " n in_size"]) -> Coeffs:
-        if self.time_in_input:
-            xs = jnp.concatenate([ts[:, None], xs], axis=1)
-
-        coeffs = diffrax.backward_hermite_coefficients(ts, xs)
-
-        return coeffs
+    history_length: eqx.AbstractVar[int]
 
     def solve(
         self,
         ts: Float[Array, " n"],
-        z0: Float[Array, " latent_size"],
-        coeffs: Coeffs,
+        xs: Float[Array, " n in_size"],
     ) -> Float[Array, " n latent_size"]:
-        ts = eqx.error_if(
-            ts, jnp.logical_not(self.valid_ts(ts)), "Invalid times provided"
-        )
-        z0 = eqx.error_if(z0, jnp.logical_not(self.valid_z0(z0)), "Invalid z0 provided")
-        coeffs = eqx.error_if(
-            coeffs,
-            jnp.logical_not(self.valid_coeffs(coeffs)),
-            "Invalid coeffs provided",
-        )
-
         if isinstance(self.solver, diffrax.AbstractAdaptiveSolver):
             stepsize_controller = diffrax.PIDController(rtol=1e-3, atol=1e-6)
             dt0 = None
@@ -119,27 +78,22 @@ class AbstractNeuralCDE[
             stepsize_controller = diffrax.ConstantStepSize()
             dt0 = jnp.nanmean(jnp.diff(ts))
 
-        control = diffrax.CubicInterpolation(ts, coeffs)
+        z0 = self.z0(ts[0], xs[0])
+        control = safe_control(ts, xs)
         term = diffrax.ControlTerm(self.term, control).to_ode()
-        saveat = diffrax.SaveAt(ts=ts)
 
         solution = diffrax.diffeqsolve(
             terms=term,
             solver=self.solver,
-            t0=jnp.nanmin(ts),
+            t0=ts[0],
             t1=jnp.nanmax(ts),
             dt0=dt0,
             y0=z0,
             stepsize_controller=stepsize_controller,
-            saveat=saveat,
         )
 
         assert solution.ys is not None
-        zs = solution.ys
-
-        zs = jnp.asarray(jnp.where(jnp.isnan(ts[:, None]), jnp.nan, zs))
-
-        return zs
+        return solution.ys[0]
 
     def z0(
         self, t0: Float[Array, ""], x0: Float[Array, " in_size"]
@@ -151,152 +105,53 @@ class AbstractNeuralCDE[
         else:
             return self.initial(x0)
 
-    @staticmethod
-    def latest_index(ts: Float[Array, " num_steps"]) -> Int[Array, ""]:
-        return jnp.nanargmax(ts)
-
     def next_state(
-        self,
-        state: NCDEState,
-        t1: Float[Array, ""],
-        x1: Float[Array, " in_size"],
-    ) -> tuple[
-        Float[Array, " num_steps"],
-        Float[Array, " num_steps input_size"],
-        Float[Array, " num_steps latent_size"],
-    ]:
+        self, state: NCDEState, ti: Float[Array, ""], xi: Float[Array, " in_size"]
+    ) -> tuple[Float[Array, " num_steps"], Float[Array, " num_steps input_size"]]:
         """Add new time and input pair to the state."""
-        ts, xs, zs = state.ts, state.xs, state.zs
-        latest_index = self.latest_index(ts)
+        ts, xs = state.ts, state.xs
+        latest_index = jnp.nanargmax(ts)
         ts = eqx.error_if(
             ts,
-            t1 <= ts[latest_index],
+            ti <= ts[latest_index],
             "new input and time must be later than all previous",
         )
 
-        def shift() -> tuple[
-            Float[Array, " num_steps"],
-            Float[Array, " num_steps input_size"],
-            Float[Array, " num_steps latent_size"],
-        ]:
+        def shift() -> (
+            tuple[Float[Array, " num_steps"], Float[Array, " num_steps input_size"]]
+        ):
             """Shift the saved times and inputs to make room for the new pair."""
             return (
-                jnp.roll(ts, -1).at[-1].set(t1),
-                jnp.roll(xs, -1, axis=0).at[-1].set(x1),
-                jnp.roll(zs, -1, axis=0).at[-1].set(jnp.nan),
+                jnp.roll(ts, -1).at[-1].set(ti),
+                jnp.roll(xs, -1, axis=0).at[-1].set(xi),
             )
 
-        def insert() -> tuple[
-            Float[Array, " num_steps"],
-            Float[Array, " num_steps input_size"],
-            Float[Array, " num_steps latent_size"],
-        ]:
+        def insert() -> (
+            tuple[Float[Array, " num_steps"], Float[Array, " num_steps input_size"]]
+        ):
             """Insert the new time and input pair at the end of the saved times and inputs."""
-            return ts.at[latest_index + 1].set(t1), xs.at[latest_index + 1].set(x1), zs
+            return ts.at[latest_index + 1].set(ti), xs.at[latest_index + 1].set(xi)
 
-        ts, xs, zs = lax.cond(
-            latest_index == self.state_size - 1,
-            shift,
-            insert,
-        )
+        ts, xs = lax.cond(latest_index == self.history_length - 1, shift, insert)
 
-        return ts, xs, zs
+        return ts, xs
 
-    # TODO: Add option to skip history during inference for speed
     def __call__(
         self,
         state: NCDEState,
-        t1: Float[Array, ""],
-        x1: Float[Array, " in_size"],
+        ti: Float[Array, ""],
+        xi: Float[Array, " in_size"],
     ) -> tuple[NCDEState, Float[Array, " out_size"]]:
-        """
-        Compute the next state and output given the current state and input.
-        """
-        ts, xs, zs = self.next_state(state, t1, x1)
-
-        if inference or self.inference:
-            z0 = self.z0(ts[0], xs[0])
-            z1 = self.solve(ts, z0, self.coeffs(ts, xs))[self.latest_index(ts)]
-        else:
-            z0 = zs[self.latest_index(ts)]
-            slice_index = jnp.min(
-                jnp.asarray([self.state_size - 2, self.latest_index(ts)])
-            )
-            sliced_ts = lax.dynamic_slice(ts, (slice_index,), (2,))
-            sliced_xs = lax.dynamic_slice(xs, (slice_index, 0), (2, self.in_size))
-
-            z1 = self.solve(sliced_ts, z0, self.coeffs(sliced_ts, sliced_xs))[1]
-
-        zs = zs.at[self.latest_index(ts)].set(z1)
-        state = NCDEState(ts, xs, zs)
-
-        return state, self.output(z1)
-
-    def t1(self, state: NCDEState) -> Float[Array, ""]:
-        """Get the last time in the state."""
-        ts = state.ts
-        return jnp.nanmax(ts)
-
-    def ts(self, state: NCDEState) -> Float[Array, " n"]:
-        """Get all times in the state."""
-        ts = state.ts
-        return ts
-
-    def x1(self, state: NCDEState) -> Float[Array, " in_size"]:
-        """Get the last input in the state."""
-        ts, xs = state.ts, state.xs
-        return xs[self.latest_index(ts)]
-
-    def xs(self, state: NCDEState) -> Float[Array, " n in_size"]:
-        """Get all inputs in the state."""
-        xs = state.xs
-        return xs
-
-    def z1(
-        self, state: NCDEState, inference: bool = False
-    ) -> Float[Array, " latent_size"]:
-        """Get the last state in the state."""
-        ts, xs, zs = state.ts, state.xs, state.zs
-
-        if inference:
-            z1 = zs[self.latest_index(ts)]
-        else:
-            z0 = self.z0(ts[0], xs[0])
-            z1 = self.solve(ts, z0, self.coeffs(ts, xs))[self.latest_index(ts)]
-
-        return z1
-
-    def zs(
-        self, state: NCDEState, inference: bool = False
-    ) -> Float[Array, " n latent_size"]:
-        """Get all states in the state."""
-
-        ts, xs, zs = state.ts, state.xs, state.zs
-
-        if not inference:
-            z0 = self.z0(ts[0], xs[0])
-            zs = self.solve(ts, z0, self.coeffs(ts, xs))
-
-        return zs
-
-    def y1(
-        self, state: NCDEState, inference: bool = False
-    ) -> Float[Array, " out_size"]:
-        """Get the last output in the state."""
-        return self.output(self.z1(state, inference))
-
-    def ys(
-        self, state: NCDEState, inference: bool = False
-    ) -> Float[Array, " n out_size"]:
-        """Get all outputs in the state."""
-        return jax.vmap(self.output)(self.zs(state, inference))
+        """Compute the next state and output given the current state and input."""
+        ts, xs = self.next_state(state, ti, xi)
+        zi = self.solve(ts, xs)
+        return NCDEState(ts, xs), self.output(zi)
 
     def reset(self) -> NCDEState:
         """Reset the state to an empty state."""
-        times = jnp.full((self.state_size,), jnp.nan, dtype=float)
-        inputs = jnp.full((self.state_size, self.in_size), jnp.nan, dtype=float)
-        states = jnp.full((self.state_size, self.latent_size), jnp.nan, dtype=float)
-        return NCDEState(times, inputs, states)
+        times = jnp.full((self.history_length,), jnp.nan, dtype=float)
+        inputs = jnp.full((self.history_length, self.in_size), jnp.nan, dtype=float)
+        return NCDEState(times, inputs)
 
 
 class MLPNeuralCDE(AbstractNeuralCDE):
@@ -311,9 +166,7 @@ class MLPNeuralCDE(AbstractNeuralCDE):
     latent_size: int
     out_size: int
 
-    state_size: int
-
-    inference: bool
+    history_length: int
 
     def __init__(
         self,
@@ -350,14 +203,14 @@ class MLPNeuralCDE(AbstractNeuralCDE):
         ] = lambda x: x,
         solver: diffrax.AbstractSolver | None = None,
         time_in_input: bool = False,
-        state_size: int = 16,
+        history_length: int = 16,
         key: Key,
     ):
         term_key, initial_key, output_key = jr.split(key, 3)
 
         self.solver = solver or diffrax.Tsit5()
         self.time_in_input = time_in_input
-        self.state_size = state_size
+        self.history_length = history_length
 
         self.in_size = in_size
         self.latent_size = latent_size
@@ -368,7 +221,7 @@ class MLPNeuralCDE(AbstractNeuralCDE):
             if term is not None
             else MLPNCDETerm(
                 input_size=in_size,
-                data_size=latent_size,
+                state_size=latent_size,
                 width_size=field_width,
                 depth=field_depth,
                 key=term_key,

lerax/model/ncde/term.py
@@ -88,7 +88,7 @@ class MLPNCDETerm(AbstractNCDETerm):
     def __init__(
         self,
         input_size: int,
-        data_size: int,
+        state_size: int,
         width_size: int,
         depth: int,
         activation: Callable[
@@ -103,8 +103,8 @@ class MLPNCDETerm(AbstractNCDETerm):
     ):
         self.add_time = add_time
         self.tensor_mlp = TensorMLP(
-            in_shape=(data_size + int(add_time),),
-            out_shape=(data_size, input_size),
+            in_shape=(state_size + int(add_time),),
+            out_shape=(state_size, input_size),
             width_size=width_size,
             depth=depth,
             activation=activation,
@@ -113,8 +113,8 @@ class MLPNCDETerm(AbstractNCDETerm):
         )
 
     def __call__(
-        self, t: ScalarLike, z: Float[Array, " data_size"], args
-    ) -> Float[Array, " data_size input_size"]:
+        self, t: ScalarLike, z: Float[Array, " state_size"], args
+    ) -> Float[Array, " state_size input_size"]:
         if self.add_time:
             return self.tensor_mlp(
                 jnp.concatenate([z, jnp.expand_dims(t, axis=-1)], axis=-1)